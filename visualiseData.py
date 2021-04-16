4bc69fba7b083489e261b103b936a62e738acc30
Craig Wilkinson <c.a.wilkinson1@googlemail.com>
2021-04-16 23:38:25 +0100

    Push trendline for time taken over distances up to a marathon
    If you've run further than 42.195, this will have no change apart from adding the gridlines to the graph to make it a bit easier to read

--- visualiseData.py
+++ visualiseData.py
@@ -1,5 +1,6 @@
 import numpy
 import pandas
+from scipy import stats
 import dateutil
 import seaborn
 import matplotlib
@@ -72,12 +73,26 @@ def produceTimePace():
 def produceElapsedTimeDistance():
     splits = databaseAccess.getSplits()
     splits = pandas.merge(splits, splits.groupby(['activity_id'])[['elapsed_time']].agg('sum'), on=["activity_id", "activity_id"])
+    splits['total_distance'] = splits['total_distance'] / 1000
+    max_distance = int(round(splits['total_distance'].max()))
+    max_time = int(round(splits['elapsed_time_y'].max()))
+    if max_distance < 42.195:
+        # Assume we want to extend for a marathon
+        max_distance = 42.195
+        # Since we haven't run that far, assume we can finish a marathon in under 5 hours
+        max_time = 18000
+    _, axes = matplotlib.pyplot.subplots()
+    xlim = [0,max_distance]
+    axes.set_xlim(xlim)
+    ylim = [0,max_time]
+    axes.set_ylim(ylim)
     matplotlib.pyplot.plot( 'total_distance', 'elapsed_time_y', data=splits, linestyle='', marker='o', markersize=5, alpha=0.1, color="blue")
-    seaborn.regplot(x = 'total_distance', y = 'elapsed_time_y', scatter=None, data = splits ,order = 2)
+    seaborn.regplot(x = 'total_distance', y = 'elapsed_time_y', scatter=None, data = splits ,order = 2, ax = axes, truncate = False)
     matplotlib.pyplot.title('Time Taken Over Distances', fontsize=18, fontweight="bold")
     matplotlib.pyplot.xticks(fontsize=16)
     matplotlib.pyplot.yticks(fontsize=16)
-    matplotlib.pyplot.xlabel('Total Distance (m)', fontsize=18)
+    matplotlib.pyplot.xlabel('Total Distance (km)', fontsize=18)
     matplotlib.pyplot.ylabel('Time Taken (sec)', fontsize=18)
+    matplotlib.pyplot.grid()
     matplotlib.pyplot.tight_layout()
     matplotlib.pyplot.savefig('Time_Taken_Distance.png')
