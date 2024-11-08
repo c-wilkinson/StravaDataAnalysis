5e074cd4606c872fb8b115cb59251526907e5d90
Craig Wilkinson <c.a.wilkinson1@googlemail.com>
2024-11-08 12:22:24 +0000

    Update getTokens.py

--- getTokens.py
+++ getTokens.py
@@ -6,18 +6,21 @@ copied_code = '[LONGCODEHERE]'
 client_id = os.environ.get('BUFFERSIZE')
 client_secret = os.environ.get('CLIENTSECRET')
 
-response = requests.post(
-                    url = 'https://www.strava.com/oauth/token',
-                    data = {
-                            'client_id': client_id,
-                            'client_secret': client_secret,
-                            'code': copied_code,
-                            'grant_type': 'authorization_code'
-                            }
-)
-
-strava_tokens = response.json()
-if 'message' in strava_tokens:
-    print(strava_tokens)
-else:
-    setConfig(strava_tokens)
+try:
+    response = requests.post(
+        url='https://www.strava.com/oauth/token',
+        data={
+            'client_id': client_id,
+            'client_secret': client_secret,
+            'code': copied_code,
+            'grant_type': 'authorization_code'
+        },
+        timeout=10  # 10 seconds timeout
+    )
+    strava_tokens = response.json()
+    if 'message' in strava_tokens:
+        print(strava_tokens)
+    else:
+        setConfig(strava_tokens)
+except requests.exceptions.Timeout:
+    print("Timeout occurred while requesting Strava tokens")
