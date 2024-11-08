bf1d8d45126327ad35c52a16ab109fa9e4c23d3b
Craig Wilkinson <c.a.wilkinson1@googlemail.com>
2024-11-08 12:23:25 +0000

    Update refreshTokens.py

--- refreshTokens.py
+++ refreshTokens.py
@@ -7,16 +7,20 @@ client_id = os.environ.get('CLIENTID')
 client_secret = os.environ.get('CLIENTSECRET')
 strava_tokens = databaseAccess.getConfig()
 if strava_tokens['expires_at'] < time.time():
-    response = requests.post(
-                        url = 'https://www.strava.com/oauth/token',
-                        data = {
-                                'client_id': client_id,
-                                'client_secret': client_secret,
-                                'grant_type': 'refresh_token',
-                                'refresh_token': strava_tokens['refresh_token']
-                                }
-                    )
-    new_strava_tokens = response.json()
-    databaseAccess.setConfig(strava_tokens)
-    # Use new Strava tokens from now
-    strava_tokens = new_strava_tokens
+    try:
+        response = requests.post(
+            url='https://www.strava.com/oauth/token',
+            data={
+                'client_id': client_id,
+                'client_secret': client_secret,
+                'grant_type': 'refresh_token',
+                'refresh_token': strava_tokens['refresh_token']
+            },
+            timeout=10  # 10 seconds timeout
+        )
+        new_strava_tokens = response.json()
+        databaseAccess.setConfig(strava_tokens)
+        # Use new Strava tokens from now
+        strava_tokens = new_strava_tokens
+    except requests.exceptions.Timeout:
+        print("Timeout occurred while refreshing Strava tokens")
