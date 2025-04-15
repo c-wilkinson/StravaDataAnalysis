866084f62d5bb729e4b3049ff67150b7480d247c
Craig Wilkinson <c.a.wilkinson1@googlemail.com>
2025-04-15 23:17:40 +0100

    Split generate readme and main (#41)

--- src/main.py
+++ src/main.py
@@ -15,7 +15,6 @@ from strava_data.db.dao import (
     load_all_activities,
     load_all_splits,
 )
-from strava_data.generate_readme import generate_readme
 from strava_data.strava_api.client import fetch_activities, fetch_splits_if_needed
 from strava_data.strava_api.processing.transform import transform_activities, transform_splits
 from strava_data.strava_api.visualisation import graphs
@@ -52,9 +51,6 @@ def main() -> None:
     all_splits = load_all_splits()
     generate_required_charts(all_activities, all_splits)
     encrypt_database()
-    LOGGER.info("Generating readme.")
-    generate_readme()
-    LOGGER.info("Readme saved.")
     LOGGER.info("Done.")
 
 
@@ -65,23 +61,41 @@ def generate_required_charts(activities_df: pd.DataFrame, splits_df: pd.DataFram
     :param activities_df: DataFrame of Strava activities.
     :param splits_df: DataFrame of 1 km splits from those activities.
     """
+    LOGGER.info("Generate Running_Pace_vs_Elevation_Change")
     graphs.plot_pace_vs_elevation_change(splits_df, "Running_Pace_vs_Elevation_Change.png")
+    LOGGER.info("Generate Time_Taken_Distance")
     graphs.plot_time_taken_over_distances(activities_df, "Time_Taken_Distance.png")
+    LOGGER.info("Generate Running_Pace_over_Time")
     graphs.plot_running_pace_over_time(splits_df, "Running_Pace_over_Time.png")
+    LOGGER.info("Generate Running_Pace_vs_Total_Distance")
     graphs.plot_pace_vs_total_distance(splits_df, "Running_Pace_vs_Total_Distance.png")
+    LOGGER.info("Generate Number_of_Runs_per_Distance")
     graphs.plot_number_of_runs_per_distance(activities_df, "Number_of_Runs_per_Distance.png")
+    LOGGER.info("Generate Fastest_1k_Pace_over_Time")
     graphs.plot_fastest_1km_pace_over_time(splits_df, "Fastest_1k_Pace_over_Time.png")
+    LOGGER.info("Generate Median_1k_Pace_over_Time")
     graphs.plot_median_1km_pace_over_time(splits_df, "Median_1k_Pace_over_Time.png")
+    LOGGER.info("Generate Total_Distance_Ran_by_Month")
     graphs.plot_total_distance_by_month(activities_df, "Total_Distance_Ran_by_Month.png")
+    LOGGER.info("Generate Pace_by_Day")
     graphs.plot_pace_by_day_of_week(splits_df, "Pace_by_Day.png")
+    LOGGER.info("Generate Activity_Heatmap")
     graphs.plot_heatmap_activities(activities_df, "Activity_Heatmap.png")
+    LOGGER.info("Generate Cumulative_Distance")
     graphs.plot_cumulative_distance_over_time(activities_df, "Cumulative_Distance.png")
+    LOGGER.info("Generate Longest_Run_per_Month")
     graphs.plot_longest_run_per_month(activities_df, "Longest_Run_per_Month.png")
+    LOGGER.info("Generate Elevation_Gain_per_KM_by_Month")
     graphs.plot_elevation_gain_per_km_by_month(activities_df, "Elevation_Gain_per_KM_by_Month.png")
+    LOGGER.info("Generate Run_Start_Time_by_Month")
     graphs.plot_run_start_time_distribution(activities_df, "Run_Start_Time_by_Month.png")
+    LOGGER.info("Generate Monthly_Distance_by_Year")
     graphs.plot_monthly_distance_by_year_grouped(activities_df, "Monthly_Distance_by_Year.png")
+    LOGGER.info("Generate Rolling_30_Day_Comparison")
     graphs.plot_rolling_distance(activities_df, "Rolling_30_Day_Comparison.png", window=30)
+    LOGGER.info("Generate Cadence_Over_Time")
     graphs.plot_cadence_over_time(activities_df, "Cadence_Over_Time.png")
+    LOGGER.info("Generate Training_Intensity_by_HeartRate_Zone")
     graphs.plot_heart_rate_zone_distribution(splits_df, "Training_Intensity_by_HeartRate_Zone.png")
 
 
